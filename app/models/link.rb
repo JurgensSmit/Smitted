5c943dabbb57cb66735bc41bbd513fa811a28826
Jurgens Smit <contact@jurgenssmit.com>
2013-05-23 18:54:03 +1000

    add associations

---
+++ app/models/link.rb
@@ -0,0 +1,6 @@
+class Link < ActiveRecord::Base
+
+	belongs_to :user
+	
+  # attr_accessible :title, :body
+end
---
+++ db/migrate/20130523083224_add_title_to_link.rb
@@ -0,0 +1,4 @@
+class AddTitleToLink < ActiveRecord::Migration
+  def change
+  end
+end
