1eb1782563a46be8cf83c49b511b7e1c97af0447
Jurgens Smit <contact@jurgenssmit.com>
2013-05-23 16:41:27 +1000

    create user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smitted::Application.config.secret_token = 'ac0b7fc94512beae620c06f4f053990364255d97a19c661f4bd9e9d03c202556b9b4c5ec8e5783927414e831de559831394b2b9f00e846a42c7ae54c26814e66'
